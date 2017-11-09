57edac62c7f58f1026a8e110e27997148cb8f709
unnieTron <unnie.ayilliath@gmail.com>
2017-11-09 15:41:31 +0100

    Sample console app querying against Azure Analysis services

---
+++ SampleConsoleAppQueryingAzureAS/SampleConsoleAppQueryingAzureAS/ADALTokenHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace SampleConsoleAppQueryingAzureAS
+{
+    class ADALTokenHelper
+    {
+        private static string _authUrl = "https://login.windows.net";
+        /// <summary>
+        /// App only access token is used to get tokens for daemon clients.
+        /// </summary>
+        /// <param name="resourceUrl"></param>
+        /// <param name="clientId"></param>
+        /// <param name="clientSecret"></param>
+        /// <returns></returns>
+        public async static Task<string> GetAppOnlyAccessToken(string domain, string resourceUrl,
+            string clientId,
+            string clientSecret)
+        {
+            var authority = $"{_authUrl}/{domain}/oauth2/token";
+            var authContext = new AuthenticationContext(authority);
+            // Config for OAuth client credentials 
+            var clientCred = new ClientCredential(clientId, clientSecret);
+            AuthenticationResult authenticationResult = await authContext.AcquireTokenAsync(resourceUrl, clientCred);
+            //get access token
+            return authenticationResult.AccessToken;
+        }
+    }
+}
---
+++ SampleConsoleAppQueryingAzureAS/SampleConsoleAppQueryingAzureAS/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AnalysisServices.AdomdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleAppQueryingAzureAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFromAzureAS().Wait();
+            Console.WriteLine("****Complete***");
+            Console.ReadKey();
+        }
+
+        private static async Task ReadFromAzureAS()
+        {
+            var clientId = "<client id of Azure AD app a.ka Service principal>";
+            var clientSecret = "<client secret>";
+            var domain = "yourdomain.onmicrosoft.com";
+            var ssasUrl = "northeurope.asazure.windows.net";//get this from your Azure AS connectionString
+            var token = await ADALTokenHelper.GetAppOnlyAccessToken(domain, $"https://{ssasUrl}", clientId, clientSecret);
+            
+            var connectionString = $"Provider=MSOLAP;Data Source=asazure://{ssasUrl}/myanalysisservice;Initial Catalog= adventureworks;User ID=;Password={token};Persist Security Info=True;Impersonation Level=Impersonate";
+            var ssasConnection = new AdomdConnection(connectionString);
+            ssasConnection.Open();
+            var query = @"Evaluate TOPN(10,Customer,Customer[Customer Id],1)";
+            var cmd = new AdomdCommand(query)
+            {
+                Connection = ssasConnection
+            };
+            using (var reader = cmd.ExecuteXmlReader())
+            {
+                string value = reader.ReadOuterXml();
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
